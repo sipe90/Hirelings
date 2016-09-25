c589bd9699b4fd60f4bfade6bc27cd50f3dbbc5f
Pasi Härme <pasi.harme@gmail.com>
2016-09-25 17:56:40 +0300

    Cleaned gradle build files

--- src/main/java/net/sipe/hirelings/HirelingsMod.java
+++ src/main/java/net/sipe/hirelings/HirelingsMod.java
@@ -19,7 +19,7 @@ import net.sipe.hirelings.proxy.AbstractProxy;
 public class HirelingsMod {
 
     public static final String MOD_ID = "hirelings";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "@VERSION@";
 
     @Mod.Instance(MOD_ID)
     public static HirelingsMod INSTANCE;
